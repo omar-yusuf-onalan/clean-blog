cda0acc186b1e2d9d14745a2f6f1aef747532325
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2022-10-11 23:09:22 +0300

    Tabs created

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    const blog = {
-        id: 1,
-        title: 'Blog title',
-        description: 'Blog description',
-    };
-    res.send(blog);
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+    res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+    res.render('post');
 });
 
 const port = 3000;
+
 app.listen(port, () => {
-    console.log(`Web Server started in port ${port}..`);
+    console.log(`Server initialized in port ${port}...`);
 });
