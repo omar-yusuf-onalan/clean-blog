25f015547583c8060565168df55a818b71335759
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2022-11-02 22:23:09 +0300

    MVC architecture implemented

--- app.js
+++ app.js
@@ -1,54 +1,46 @@
 const express = require('express');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 const ejs = require('ejs');
-
 const path = require('path');
-
 const Post = require('./models/Post');
+
+const postController = require('./controllers/postController');
+const pageController = require('./controllers/pageController');
 
 const app = express();
 
+//connect DB
 mongoose.connect('mongodb://localhost/cleanblog-test-db', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
+//TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
+//MIDDLEWARES
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
+app.use(
+    methodOverride('_method', {
+        methods: ['POST', 'GET'],
+    })
+);
 
-app.get('/', async (req, res) => {
-    const posts = await Post.find({});
-    res.render('index', {
-        posts,
-    });
-});
+//ROUTES
+app.get('/', postController.getAllPosts);
+app.get('/posts/:id', postController.getPost);
+app.post('/posts', postController.createPost);
+app.put('/posts/:id', postController.updatePost);
+app.delete('/posts/:id', postController.deletePost);
 
-app.get('/about', (req, res) => {
-    res.render('about');
-});
-
-app.get('/add_post', (req, res) => {
-    res.render('add_post');
-});
-
-app.get('/post', (req, res) => {
-    res.render('post');
-});
-
-app.get('/posts/:id', async (req, res) => {
-    const post = await Post.findById(req.params.id);
-    res.render('post', {
-        post,
-    });
-});
-
-app.post('/posts', async (req, res) => {
-    await Post.create(req.body);
-    res.redirect('/');
-});
+app.get('/about', pageController.getAboutPage);
+app.get('/add_post', pageController.getAddPage);
+app.get('/posts/edit/:id', pageController.getEditPage);
 
 const port = 3000;
 
